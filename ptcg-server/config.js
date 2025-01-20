0702df9ef95e6dbe27142c4fbc25f9e259992f82
Marco Escher <mrcschr@gmail.com>
2025-01-20 23:42:52 +0100

    Setup

--- ptcg-server/config.js
+++ ptcg-server/config.js
@@ -1,6 +1,6 @@
 const { config } = require('./output/config');
 
-config.backend.address = '0.0.0.0';
+config.backend.address = '127.0.0.1';
 config.backend.port = 8080;
 config.backend.avatarsDir = __dirname + '/avatars';
 
